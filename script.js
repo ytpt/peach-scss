0d339581c6f76989287ce077609476307ae2c026
Annarchive <ytpt@mail.ru>
2023-02-20 22:51:53 +0800

    add desktop view to map__header block in style.scss; add underline code to script.js

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true
 });
+
+// const REGION_ACTIVE = document.querySelector('.active');
+// const UNDERLINE_ELEM = document.createElement('div');
+// UNDERLINE_ELEM.classList.add('underline');
+// document.querySelector('.map__header').appendChild(UNDERLINE_ELEM);
+
+const REGION_BUTTONS = document.querySelectorAll('.map__nav-item');
+REGION_BUTTONS.forEach(region => {
+    region.addEventListener('click', function() {
+        REGION_BUTTONS.forEach(region_btn => {
+            region_btn.addEventListener('click', function() {
+                if (!region_btn.classList.contains('active')) {
+                    document.querySelector('.active').classList.remove('active');
+                    region_btn.classList.add('active');
+                }
+            })
+        })
+    })
+})
