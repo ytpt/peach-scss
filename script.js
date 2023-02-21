eb1bdc952380f929dfb508cd5210cde7fc3bcd4f
Annarchive <ytpt@mail.ru>
2023-02-22 01:05:28 +0800

    add styles for desktop solutions block in style.scss; add solutions_items function in script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true
-});
-
+//Map
 const REGION_BUTTONS = document.querySelectorAll('.map__nav-item');
 REGION_BUTTONS.forEach(btn => {
     btn.addEventListener('click', function() {
@@ -10,3 +6,27 @@ REGION_BUTTONS.forEach(btn => {
         btn.classList.add('active');
     })
 })
+
+//Solutions
+const SOLUTIONS_ITEMS = document.querySelectorAll('.solutions__nav-item');
+if (document.documentElement.clientWidth > 1200) {
+    SOLUTIONS_ITEMS.forEach(item => {
+        item.addEventListener('mouseover', function() {
+            item.classList.add('hover');
+            item.querySelector('.solutions__nav-subtitle').style.display = 'block';
+            item.querySelector('h3').style.alignSelf = 'start';
+        })
+        item.addEventListener('mouseout', function() {
+            item.classList.remove('hover');
+            item.querySelector('.solutions__nav-subtitle').style.display = 'none';
+            item.querySelector('h3').style.alignSelf = 'end';
+        })
+    })
+}
+
+//Slider
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true
+});
+
